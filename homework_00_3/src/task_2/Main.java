86eecddc201d3ca0e3202bc9f11adf47b3152c73
oleksiuk-petro <oleksiuk.petro1993@gmail.com>
2021-09-17 18:47:37 +0300

    I rewrote homework_00_3.

--- homework_00_3/src/task_2/Main.java
+++ homework_00_3/src/task_2/Main.java
@@ -2,6 +2,7 @@
 Створити стрічкову змінну яка зберігатиме день тижня. Використовуючи конструкцію switch скласти розклад на тиждень. В залежності від дня тижня у користувача на екрані відображається те, ща заплановано на цей день.
  */
 package task_2;
+
 import java.util.Scanner;
 
 public class Main {
@@ -16,15 +17,30 @@ public class Main {
         int number = num_scan.nextInt();
 
         //оператор вибору switch
-        switch (number){
-            case 1: day="Monday - swimming!";break;
-            case 2: day="Tuesday - running!";break;
-            case 3: day="Wednesday - jumping!";break;
-            case 4: day="Thursday - cycling!";break;
-            case 5: day="Friday - climbing!";break;
-            case 6: day="Saturday - cleaning!";break;
-            case 7: day="Sunday - vacation!";break;
-            default: day="It is wrong number day!";
+        switch (number) {
+            case 1:
+                day = "Monday - swimming!";
+                break;
+            case 2:
+                day = "Tuesday - running!";
+                break;
+            case 3:
+                day = "Wednesday - jumping!";
+                break;
+            case 4:
+                day = "Thursday - cycling!";
+                break;
+            case 5:
+                day = "Friday - climbing!";
+                break;
+            case 6:
+                day = "Saturday - cleaning!";
+                break;
+            case 7:
+                day = "Sunday - vacation!";
+                break;
+            default:
+                day = "It is wrong number day!";
         }
         System.out.print(day);
     }
